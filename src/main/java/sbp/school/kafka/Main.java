051dc8671d71f50b1469a2747f986b1d8cec1c67
Alex <novikov.v.alexandr@gmail.com>
2024-12-21 14:33:06 +0300

    Задание 1

--- src/main/java/sbp/school/kafka/Main.java
+++ src/main/java/sbp/school/kafka/Main.java
@@ -2,6 +2,6 @@ package sbp.school.kafka;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Run with java -cp target\\kafka.jar sbp.school.kafka.Task1");
     }
 }
