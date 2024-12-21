126907cba5ece1cc6a3daa05504fda5778287753
nov-alex <2564794+nov-alex@users.noreply.github.com>
2024-12-21 14:05:26 +0300

    Задание 1

--- src/main/java/sbp/school/kafka/Main.java
+++ src/main/java/sbp/school/kafka/Main.java
@@ -1,78 +1,7 @@
 package sbp.school.kafka;
 
-import com.networknt.schema.JsonSchema;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import sbp.school.kafka.config.ProducerConfiguration;
-import sbp.school.kafka.entity.TransactionData;
-import sbp.school.kafka.entity.TransactionType;
-import sbp.school.kafka.service.ProducerService;
-import sbp.school.kafka.util.json.TransactionDataJsonValidator;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.*;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            List<TransactionData> data = generateTransactionData();
-            String topic = ProducerConfiguration.getProducerTopicConfig().getProperty("topic");
-            JsonSchema jsonSchema = TransactionDataJsonValidator.readJsonSchema();
-            TransactionDataJsonValidator jsonValidator = new TransactionDataJsonValidator(jsonSchema);
-            try (KafkaProducer<String, TransactionData> producer = new KafkaProducer<>(ProducerConfiguration.getProducerConfig())) {
-                ProducerService service = new ProducerService(topic,
-                        producer,
-                        jsonValidator);
 
-                data.forEach(service::send);
-                producer.flush();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println(Arrays
-                    .stream(e.getStackTrace())
-                    .map(s -> s + "\n")
-                    .collect(Collectors.joining()));
-        }
-    }
-
-    /**
-     * Данные для отправки в кафку
-     *
-     * @return список объектов для отправки
-     */
-
-    private static List<TransactionData> generateTransactionData() {
-        Supplier<String> accountNumberGenerator = () -> {
-            StringBuilder str = new StringBuilder();
-            Random random = new Random();
-            for (int i = 0; i < 20; i++) {
-                str.append(random.nextInt(10));
-            }
-            return str.toString();
-        };
-
-        List<TransactionData> data = new ArrayList<>();
-        data.add(new TransactionData(UUID.randomUUID(),
-                TransactionType.TRANSACTION_CREDIT,
-                new BigDecimal("50.01"),
-                LocalDate.now(),
-                accountNumberGenerator.get()));
-
-        data.add(new TransactionData(UUID.randomUUID(),
-                TransactionType.TRANSACTION_DEBIT,
-                new BigDecimal("50.02"),
-                LocalDate.now(),
-                accountNumberGenerator.get()));
-
-        data.add(new TransactionData(UUID.randomUUID(),
-                TransactionType.TRANSACTION_CHARGEBACK,
-                new BigDecimal("50.03"),
-                LocalDate.now(),
-                accountNumberGenerator.get()));
-
-        return data;
     }
 }
