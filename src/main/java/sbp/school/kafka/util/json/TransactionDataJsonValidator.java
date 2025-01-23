28c51424c40860c5089bfc4142b9c81a8f02e205
nov-alex <2564794+nov-alex@users.noreply.github.com>
2025-01-23 11:20:27 +0300

    Задание 2 (#10)
    Задание 2

--- src/main/java/sbp/school/kafka/util/json/TransactionDataJsonValidator.java
+++ src/main/java/sbp/school/kafka/util/json/TransactionDataJsonValidator.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.schema.JsonSchema;
 import com.networknt.schema.ValidationMessage;
-import sbp.school.kafka.exception.ApplicationSerializationException;
+import sbp.school.kafka.exception.ApplicationValueDeserializerException;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -13,26 +13,25 @@ import java.io.InputStream;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Валидатор схемы json
+ */
 public class TransactionDataJsonValidator {
 
     private static final String TRANSACTION_DATA_SCHEMA = "transactiondata-schema.json";
-    private final JsonSchema jsonSchema;
+    private static final JsonSchema jsonSchema = readJsonSchema();
 
-    public TransactionDataJsonValidator(JsonSchema jsonSchema) {
-        this.jsonSchema = jsonSchema;
-    }
-
-    public void validate(String jsonTransactionData) throws ApplicationSerializationException {
+    public static void validate(String jsonTransactionData) throws ApplicationValueDeserializerException {
         JsonNode jsonNode;
         ObjectMapper mapper = ObjectMapperJson.getObjectMapper();
         try {
             jsonNode = mapper.readTree(jsonTransactionData);
         } catch (JsonProcessingException e) {
-            throw new ApplicationSerializationException(e);
+            throw new ApplicationValueDeserializerException(e);
         }
         Set<ValidationMessage> errors = jsonSchema.validate(jsonNode);
         if (!errors.isEmpty()) {
-            throw new ApplicationSerializationException("Валидация json: " + errors
+            throw new ApplicationValueDeserializerException("Валидация json: " + errors
                     .stream()
                     .map(ValidationMessage::toString)
                     .collect(Collectors.joining(";")));
@@ -43,15 +42,15 @@ public class TransactionDataJsonValidator {
         return TransactionDataJsonValidator.class.getClassLoader().getResourceAsStream(TRANSACTION_DATA_SCHEMA);
     }
 
-    public static JsonSchema readJsonSchema() {
+    private static JsonSchema readJsonSchema() {
         try (InputStream inputStream = getInputStream()) {
             if (inputStream == null) {
-                throw new ApplicationSerializationException("Не удается открыть файл " + TRANSACTION_DATA_SCHEMA);
+                throw new ApplicationValueDeserializerException("Не удается открыть файл " + TRANSACTION_DATA_SCHEMA);
             }
             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
             return SchemaFactoryJson.getJsonSchemaFactory().getSchema(bufferedInputStream);
         } catch (IOException e) {
-            throw new ApplicationSerializationException(e);
+            throw new ApplicationValueDeserializerException(e);
         }
     }
 }
